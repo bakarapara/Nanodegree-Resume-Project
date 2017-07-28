010d0eb075004fa6aee6b5a1a81163415debf938
Rasim <rasim.rakhmanov@gmail.com>
2017-07-28 17:02:08 +0400

    bugfix

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -74,11 +74,11 @@ var bio = {
     "role": "UI designer and developer",
     "welcomeMessage": "Maecenas tristique a dui ut varius. Etiam a purus porttitor, lobortis ipsum ac, finibus nibh. Ut tempor sed metus vitae sagittis. Mauris nec elementum sem.",
     "contacts": {
-		        "mobile": "+994 55 757 96 06",
-		        "email": "rasim@magnum.az",
-		        "github": "http://github.com/rakhmanovr",
-		        "twitter": "@rasim_rakhmanov",
-		        "location": "Moscow, Russia"
+            "mobile": "+994 55 757 96 06",
+            "email": "rasim@magnum.az",
+            "github": "http://github.com/rakhmanovr",
+            "twitter": "@rasim_rakhmanov",
+            "location": "Moscow, Russia"
     },
     "biopic": "images/me.jpg",
     "skills": ["Graphic Design", "UI/UX", "Photography", "HTML/CSS", "Java Script"]
@@ -226,7 +226,7 @@ function displayEducation() {
 
     for (var x = 0; x < education.onlineCourses.length; x++) {
         $("#education").append(HTMLschoolStart);
-        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[x].title).replace("#", education.onlineCourses[i].url);
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[x].title).replace("#", education.onlineCourses[x].url);
         var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[x].school);
         var formattedOnlineSchoolTitle = formattedOnlineTitle + formattedOnlineSchool;
         $(".education-entry:last").append(formattedOnlineSchoolTitle);
